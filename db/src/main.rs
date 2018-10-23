5f20e9796b2957955672a9532093549a0f17831d
Andrew Houts <ahouts4@gmail.com>
2018-10-23 11:54:03 -0700

    refactor db so disel_cli is not required

---
+++ db/src/main.rs
@@ -0,0 +1,46 @@
+#![allow(proc_macro_derive_resolution_fallback)]
+
+#[macro_use]
+extern crate clap;
+#[macro_use]
+extern crate diesel;
+extern crate fn_search_backend;
+extern crate diesel_migrations;
+
+pub mod schema;
+pub mod models;
+#[cfg(test)]
+mod tests;
+
+use diesel::prelude::*;
+use diesel::pg::PgConnection;
+use diesel_migrations::run_pending_migrations;
+use fn_search_backend::get_config;
+use std::error::Error;
+
+pub fn establish_connection(f: &str) -> Result<PgConnection, Box<Error>> {
+    let cfg = get_config(f)?.db;
+    let database_url = format!("postgres://{}:{}@{}/{}", cfg.user, cfg.password, cfg.host, cfg.db);
+    Ok(PgConnection::establish(&database_url)?)
+}
+
+pub fn run_migrations(cfg_file: &str) -> Result<(), Box<Error>> {
+    let connection = establish_connection(cfg_file)?;
+    run_pending_migrations(&connection)?;
+    Ok(())
+}
+
+fn main() {
+    let matches = clap_app!(myapp =>
+        (version: crate_version!())
+        (author: crate_authors!())
+        (about: crate_description!())
+        (@arg CONFIG: -c --config +takes_value +required "configuration file")
+    ).get_matches();
+
+    let cfg_file = matches.value_of("CONFIG").expect("error parsing configuration file");
+    run_migrations(cfg_file).map_err(|e| {
+        println!("error while running migrations: {}", e);
+        ()
+    }).unwrap(); // panic on error
+}

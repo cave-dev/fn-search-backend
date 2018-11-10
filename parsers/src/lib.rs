eb06c10e1ee8c58ef100daf5b924f8b5ac3ba061
Yuya Yabe <yuyabee@gmail.com>
2018-11-10 10:41:00 -0800

    Works with the test cases

--- parsers/src/lib.rs
+++ parsers/src/lib.rs
@@ -1,8 +1,11 @@
 #[macro_use]
 extern crate nom;
 
+use nom::types::CompleteStr;
+use nom::eof;
+
 #[derive(Debug, PartialEq)]
-enum ElmExpose<'a> {
+enum ElmModule<'a> {
     All,
     List(Vec<TypeOrFunction<'a>>),
 }
@@ -29,42 +32,71 @@ struct Function<'a> {
     type_signature: Option<TypeSignature<'a>>,
 }
 
-fn is_space(s: char) -> bool {
-    s == ' '
+fn is_space_or_newline(c: char) -> bool {
+    c.is_whitespace() || c == '\n'
 }
 
-named!(expose_all<&str, ElmExpose>,
-    map!(tag!(".."), |_| ElmExpose::All)
+fn is_alphanumeric(c: char) -> bool {
+    c.is_alphanumeric()
+}
+
+fn is_space_or_newline_or_comma(c: char) -> bool {
+    is_space_or_newline(c) || c == ','
+}
+
+named!(expose_all<&str, ElmModule>,
+    map!(tag!(".."), |_| ElmModule::All)
 );
 
-named!(function<&str, TypeOrFunction>,
-    map!(delimited!(char!(' '), take_till!(is_space), char!(' ')),
-        |s: &str| TypeOrFunction::Function(Function{name: s, type_signature: None})
+named!(expose_functions_and_types<&str, ElmModule>,
+    map!(
+        separated_list!(tag!(","), function_or_type),
+        ElmModule::List
     )
 );
 
-named!(type_<&str, TypeOrFunction>,
-    map!(take_till!(is_space),
-        |s: &str| TypeOrFunction::Type(Type{name: s, definition: None})
+named!(function_or_type<&str, TypeOrFunction>,
+    map!(
+        delimited!(
+            take_while!(is_space_or_newline),
+            take_while!(is_alphanumeric),
+            take_while!(is_space_or_newline)
+        ),
+        |s| {
+            // based on the assumption that anything starting with:
+            //      lowercase is a function
+            //      uppcase is a type
+            if s.chars().next().unwrap().is_lowercase() {
+                TypeOrFunction::Function(
+                    Function{
+                        name: s,
+                        type_signature: None
+                    }
+                )
+            } else {
+                TypeOrFunction::Type(
+                    Type{
+                        name: s,
+                        definition: None
+                    }
+                )
+            }
+        }
     )
 );
 
-named!(expose_functions_and_types<&str, ElmExpose>,
-    map!(separated_nonempty_list!(tag!(","), alt!(function | type_)), ElmExpose::List)
+named!(multi_spaces_or_new_line_or_comma<&str, &str>,
+    map!(take_while!(is_space_or_newline_or_comma), |s| s)
 );
 
-named!(multispaces<&str, &str>,
-    map!(is_a!(" "), |s| s)
-);
-
-named!(elm<&str, ElmExpose>,
+named!(elm<&str, ElmModule>,
     do_parse!(
         tag!("module") >>
-        multispaces >>
-        take_till!(is_space) >>
-        multispaces >>
+        multi_spaces_or_new_line_or_comma >>
+        take_till!(is_space_or_newline_or_comma) >>
+        multi_spaces_or_new_line_or_comma >>
         tag!("exposing") >>
-        multispaces >>
+        multi_spaces_or_new_line_or_comma >>
         char!('(') >>
         exposed: alt!(expose_all | expose_functions_and_types) >>
         char!(')') >>
@@ -72,39 +104,103 @@ named!(elm<&str, ElmExpose>,
     )
 );
 
-#[test]
-fn expose_all_works() {
-    assert_eq!(
-        elm("module Main exposing (..)"),
-        Ok(("",
-            ElmExpose::All
-        ))
-    );
-}
+#[cfg(test)]
+mod tests {
+    use super::*;
 
-#[test]
-fn expose_one_type_works() {
-    assert_eq!(
-        elm("module Main exposing (Test0)"),
-        Ok(("",
-            ElmExpose::List(
-                vec!(TypeOrFunction::Function(Function{name: "Test0", type_signature: None}))
-            )
-        ))
-    );
-}
+    #[test]
+    fn expose_all_works() {
+        assert_eq!(
+            elm("module Main exposing (..)"),
+            Ok(("",
+                ElmModule::All
+            ))
+        );
+    }
 
-#[test]
-fn expose_many_types_works() {
-    assert_eq!(
-        elm("module Main exposing ( Test0, Test1 )"),
-        Ok(("",
-            ElmExpose::List(
-                vec!(
-                    TypeOrFunction::Function(Function{name: "Test0", type_signature: None}),
-                    TypeOrFunction::Function(Function{name: "Test1", type_signature: None})
+    #[test]
+    fn expose_one_type_works() {
+        assert_eq!(
+            elm("module Main exposing (test0)"),
+            Ok(("",
+                ElmModule::List(
+                    vec!(
+                        TypeOrFunction::Function(
+                            Function{name: "test0", type_signature: None}
+                        ),
+                    )
                 )
-            )
-        ))
-    );
+            ))
+        );
+    }
+
+    #[test]
+    fn test_my_expectations() {
+        // named!(f<&str, TypeOrFunction>, alt!(function | type_));
+        // named!(g<&[u8], Vec<&[u8]>>, separated_list!(tag!(","), is_not!("")));
+
+        // assert_eq!(
+            // g(&b"(Test0)"[..]),
+            // Ok((&b""[..], vec!()))
+        // );
+
+        // assert_eq!(
+            // function("Test0"),
+            // // Ok(("", ElmModule::All))
+            // Ok(("", TypeOrFunction::Function( Function{name: "Test0", type_signature: None})))
+        // );
+
+        named!(s<&str, &str>, ws!(take_while!(is_alphanumeric)));
+
+        assert_eq!(
+            s(" Test0\0"),
+            // Ok(("", ElmModule::All))
+            Ok(("\u{0}", "Test0"))
+        );
+    }
+
+    #[test]
+    fn expose_many_types_works() {
+        assert_eq!(
+            elm("module Main exposing (Test0, test1)"),
+            Ok(("",
+                ElmModule::List(
+                    vec!(
+                        TypeOrFunction::Type(
+                            Type{name: "Test0", definition: None}
+                        ),
+                        TypeOrFunction::Function(
+                            Function{name: "test1", type_signature: None}
+                        ),
+                    )
+                )
+            ))
+        );
+    }
+
+    #[test]
+    fn newline_separator() {
+        assert_eq!(
+            elm("module Utils.Time\n   exposing\n  ( a\n , b\n , c\n , d\n   )"),
+
+            Ok(("",
+                ElmModule::List(
+                    vec!(
+                        TypeOrFunction::Function(
+                            Function{name: "a", type_signature: None}
+                        ),
+                        TypeOrFunction::Function(
+                            Function{name: "b", type_signature: None}
+                        ),
+                        TypeOrFunction::Function(
+                            Function{name: "c", type_signature: None}
+                        ),
+                        TypeOrFunction::Function(
+                            Function{name: "d", type_signature: None}
+                        ),
+                    )
+                )
+            ))
+        );
+    }
 }

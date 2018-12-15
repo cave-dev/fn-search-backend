57456813fb3d9bf1b2176a98d97fecfca3fd0365
Yuya Yabe <yuyabee@gmail.com>
2018-12-14 19:44:33 -0800

    WIP: implementing parsers for definitions of types, typesignatures of functions and comments

--- parsers/src/helpers.rs
+++ parsers/src/helpers.rs
@@ -9,3 +9,7 @@ pub fn is_alphanumeric(c: char) -> bool {
 pub fn is_space_or_newline_or_comma(c: char) -> bool {
     is_space_or_newline(c) || c == ','
 }
+
+pub fn is_space_or_newline_or_(c: char) -> bool {
+    is_space_or_newline(c) || c == ','
+}

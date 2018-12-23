83f4176992553dc5bc57796f2bac78dade8f4f92
Yuya Yabe <yuyabee@gmail.com>
2018-12-22 21:11:48 -0800

    Handles operators

--- parsers/src/helpers.rs
+++ parsers/src/helpers.rs
@@ -6,8 +6,12 @@ pub fn is_alphanumeric(c: char) -> bool {
     c.is_alphanumeric() || c == '_'
 }
 
+pub fn is_operator(c: char) -> bool {
+    ['+', '-', '/', '*', '^', '=', '>', '<', '&', '|'].iter().any(|&x| x == c)
+}
+
 pub fn is_allowed_for_types_and_functions(c: char) -> bool {
-    is_alphanumeric(c) || c == '.' || c == ','
+    is_alphanumeric(c) || ['.', ','].iter().any(|&x| x == c)
 }
 
 pub fn is_space_or_newline_or_comma(c: char) -> bool {
